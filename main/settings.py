7d90d1d0c4e1ae102289f9d785ffa2b8fb6e17e3
abellamas <abellamasm@gmail.com>
2023-10-22 20:33:07 -0300

    index with "hola mundo"

--- main/settings.py
+++ main/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "fontawesomefree",
+    "bootstrap5",
     "sw_acoustic_iso"
 ]
 
@@ -70,7 +72,7 @@ ROOT_URLCONF = "main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- sw_acoustic_iso/views.py
+++ sw_acoustic_iso/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from sw_acoustic_iso.models import Materials
 
 # Create your views here.
+def index(request):
+    
+    materials = Materials.objects.all()
+    
+    return render(request, 'base.html')

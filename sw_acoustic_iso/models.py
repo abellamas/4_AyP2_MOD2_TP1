fe10e09ad9cd57cc2b20d4f9406eb1e743640855
abellamas <abellamasm@gmail.com>
2023-10-22 20:06:34 -0300

    load_csv terminado

--- sw_acoustic_iso/models.py
+++ sw_acoustic_iso/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Materials(models.Model):
+    material = models.CharField(verbose_name="Material", max_length=100)
+    density = models.IntegerField(verbose_name="Densidad")
+    young_module = models.FloatField(verbose_name="Módulo de Young")
+    loss_factor = models.FloatField(verbose_name="Factor de perdidas")
+    poisson_module = models.FloatField(verbose_name="Módulo de Poisson")

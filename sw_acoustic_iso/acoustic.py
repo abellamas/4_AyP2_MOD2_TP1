48a59fda214428abde7d1b6218fca71c0720802d
abellamas <abellamasm@gmail.com>
2023-10-23 01:49:28 -0300

    equations working

--- sw_acoustic_iso/acoustic.py
+++ sw_acoustic_iso/acoustic.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
-class Material():
-    def __init__(self, material):
-        self.__name = material.iloc[0]
-        self.__density = material.iloc[1]
-        self.__mod_young = material.iloc[2]
-        self.__n_int = material.iloc[3]
-        self.__mod_poisson = material.iloc[4]
+class Panel():
+    def __init__(self, name, density, young_module, loss_factor, poisson_module, lx, ly, thickness):
+        self.__name = name
+        self.__density = float(density)
+        self.__young_module = float(young_module)
+        self.__loss_factor = float(loss_factor)
+        self.__poisson_module = float(poisson_module)
+        self.__lx = float(lx)
+        self.__ly = float(lx)
+        self.__thickness = float(thickness)
         self.__vel_sound_air = 343
-        self.__lx = 0
-        self.__ly = 0
-        self.__thickness = 0
         self.__stiffness = 0
         self.__mass_sup = 0
         self.__freq_critic = 0
@@ -52,7 +52,7 @@ class Material():
     @property
     def stiffness(self):
         if self.__thickness != 0:
-            self.__stiffness = (self.__mod_young/(1-self.__mod_poisson**2))*self.__thickness**3/12
+            self.__stiffness = (self.__young_module/(1-self.__poisson_module**2))*self.__thickness**3/12
             return self.__stiffness
         else:
             raise('Thickness cant be zero')
@@ -81,7 +81,7 @@ class Material():
     
     
     def data(self):
-        return f'Material: {self.__name}\n Densidad: {self.__density}\n Módulo de Young: {self.__mod_young}\n Factor de pérdidas: {self.__n_int}\n Módulo de Poisson: {self.__mod_poisson}'
+        return f'Material: {self.__name}\n Densidad: {self.__density}\n Módulo de Young: {self.__young_module}\n Factor de pérdidas: {self.__loss_factor}\n Módulo de Poisson: {self.__poisson_module}'
     
     def cramer_model(self, frecuencies):
         f_analysis = np.append(frecuencies, self.__freq_critic)
@@ -93,10 +93,10 @@ class Material():
                 r = 20*np.log10(self.__mass_sup*f) - 47 
                 r_vs_freq.append(r)
             elif f == self.__freq_critic:
-                r = 20*np.log10(self.__mass_sup*f) - 47 - 10*np.log10(np.pi/(4*(self.__n_int+self.__mass_sup/(485*np.sqrt(f))))) 
+                r = 20*np.log10(self.__mass_sup*f) - 47 - 10*np.log10(np.pi/(4*(self.__loss_factor+self.__mass_sup/(485*np.sqrt(f))))) 
                 r_vs_freq.append(r)
             else:
-                r = 20*np.log10(self.__mass_sup*f) - 47 - 10*np.log10(np.pi/4*(self.__n_int+self.__mass_sup/(485*np.sqrt(f)))) + 10*np.log10(f/self.__freq_critic) + 10*np.log10(1-self.__freq_critic/f) 
+                r = 20*np.log10(self.__mass_sup*f) - 47 - 10*np.log10(np.pi/4*(self.__loss_factor+self.__mass_sup/(485*np.sqrt(f)))) + 10*np.log10(f/self.__freq_critic) + 10*np.log10(1-self.__freq_critic/f) 
                 r_vs_freq.append(r)
                 
         return f_analysis, r_vs_freq

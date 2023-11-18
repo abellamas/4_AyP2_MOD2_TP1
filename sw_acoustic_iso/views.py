6204a5f94cdf506b62f5b828472d01e43ba38fa8
abellamas <abellamasm@gmail.com>
2023-11-18 18:36:13 -0300

    configuracion plotly_white con cremer y davy

--- sw_acoustic_iso/views.py
+++ sw_acoustic_iso/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from sw_acoustic_iso.models import Materials
 from sw_acoustic_iso.forms import MaterialsPanelForm, DimensionsPanel
 from sw_acoustic_iso.acoustic import Panel
-import plotly.express as px
+import plotly.graph_objects as go
 
 # Create your views here.
 def index(request):
@@ -11,18 +11,41 @@ def index(request):
         material_form = MaterialsPanelForm(request.POST)
         dimensions_form = DimensionsPanel(request.POST)
         if material_form.is_valid() & dimensions_form.is_valid():
+            
             material = Materials.objects.get(id=request.POST.get('material'))
             lx = request.POST.get('l_x')
             ly = request.POST.get('l_y')
             thickness = request.POST.get('thickness')
+            
             panel = Panel(material.material, material.density, material.young_module, material.loss_factor, material.poisson_module, lx, ly, thickness)
+            
             f_per_thirds = [20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000]
+            
             f_cremer, r_cremer = panel.cremer_model(f_per_thirds)
-            fig = px.line(x=f_cremer, y=r_cremer, log_x=True, color_discrete_sequence=['red'], labels={'x': 'Frecuencia [Hz]', 'y': 'R [dB]'})
-            fig.update_layout(legend_title_text='Modelo de Cremer')
-            fig.update_layout(autosize=True)
-            fig.update_xaxes(tickvals=f_per_thirds, ticktext=f_per_thirds)
+            f_davy, r_davy = panel.davy_model() 
+            
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=f_cremer, y=r_cremer, name="Modelo de Cremer"), )
+            fig.add_trace(go.Scatter(x=f_davy, y=r_davy, name="Modelo de Davy", visible="legendonly"))
+            # fig = px.line(x=f_cremer, y=r_cremer, log_x=True, color_discrete_sequence=['red'], labels={'x': 'Frecuencia [Hz]', 'y': 'R [dB]'})
+            # fig.update_layout(legend_title_text='Aislamiento ', legend, autosize=True)
+            # fig.update_xaxes(tickvals=f_per_thirds, ticktext=f_per_thirds)
+            fig.update_xaxes(type="log", tickvals=f_per_thirds, ticktext=f_per_thirds)
+            fig.update_layout(
+                legend=dict(
+                    orientation="h",
+                    # entrywidth=70,
+                    yanchor="bottom",
+                    y=1.02,
+                    xanchor="right",
+                    x=1
+                ),
+                template = 'plotly_white'
+            )
+            
             fig_html = fig.to_html()
+
+            
             return render(request, 'base.html', {'material_form': material_form, 'dimensions_form': dimensions_form, 'fig_html' : fig_html, 'stiffness' : panel.stiffness, 'mass_sup':panel.mass_sup, 'freq_res': panel.freq_res, 'freq_critic': panel.freq_critic})
             
         else:

d109c565d2b7315f37edf0d9fd5b8846dfb10974
abellamas <abellamasm@gmail.com>
2023-11-19 17:47:37 -0300

    1er version ISO 12354-1

--- sw_acoustic_iso/acoustic.py
+++ sw_acoustic_iso/acoustic.py
@@ -298,13 +298,172 @@ class Panel():
             reduction.append(transmission_lost)
             
         return f_analysis, reduction
+
+    # def iso_12354(self):
+    #     ro0 = self.__density_air # Densidad del aire [kg/m^3]
+    #     c0 = self.__vel_sound_air # Velocidad del sonido [m/s]
+    #     freq_tercio = np.array([20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160,
+    #                             200, 250, 315, 400, 500, 630, 800, 1000, 1250,
+    #                             1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000,
+    #                             10000, 12500, 16000, 20000])    
+    #     espesor = float(self.ui.espesor.text())
+    #     alto = float(self.ui.alto.text())
+    #     largo = float(self.ui.largo.text())
+ 
+    #     R_iso = []
+    #     if self.__lx > self.__ly: # siempre l1 > l2 
+    #         l1 = largo
+    #         l2 = alto
+    #     else:
+    #         l1 = alto
+    #         l2 = largo 
+    #     for f in freq_tercio: 
+    #         def etatotal(f):
+    #             eta_tot = eta + m/(485*np.sqrt(f))
+    #             return eta_tot
+    #         def delta1(f): 
+    #             lamb = np.sqrt(f/fc)
+    #             delta_1 = (((1 - lamb**2)*np.log((1+lamb)/(1-lamb)) + 2*lamb)/(4*(np.pi**2)*(1-lamb**2)**1.5))
+    #             return delta_1
+    #         def delta2(f):
+    #             lamb = np.sqrt(f/fc)
+    #             delta_2 = (8*(c0**2)*(1 - 2*lamb**2))/((fc**2)*(np.pi**4)*l1*l2*lamb*np.sqrt(1 - lamb**2))
+    #             return delta_2
+    #         def radforzada(f): 
+    #             Lambda = - 0.964 - (0.5 + l2/(np.pi*l1))*np.log(l2/l1) + ((5*l2)/(2*np.pi*l1)) - (1/(4*np.pi*l1*l2*((2*np.pi*f)/c0)**2))
+    #             sigma = 0.5*(np.log(((2*np.pi*f)/c0)*np.sqrt(l1*l2)) - Lambda) # Factor de radiación para ondas forzadas
+    #             return sigma
+    #         def sigma1(f):
+    #             sigma_1 = 1/(np.sqrt(1 - fc/f)) 
+    #             return sigma_1
+    #         def sigma2(f):
+    #             sigma_2 = 4*l1*l2*(f/c0)**2
+    #             return sigma_2
+    #         def sigma3(f):
+    #             sigma_3 = np.sqrt((2*np.pi*f*(l1+l2))/(16*c0))
+    #             return sigma_3
+            
+    #         if f > fc/2:
+    #             delta_2 = 0
+    #         else:
+    #             delta_2 = delta2(f)
+    #         # Calculamos el factor de radiación por ondas libres  
+    #         if f11 <= fc/2: 
+    #             if f >= fc:
+    #                 rad_libre = sigma1(f)    
+    #             elif f < fc:
+    #                 lamb = np.sqrt(f/fc)
+    #                 delta_1 = (((1 - lamb**2)*np.log((1+lamb)/(1-lamb)) + 2*lamb)/(4*(np.pi**2)*(1-lamb**2)**1.5))
+    #                 delta_2 = (8*(c0**2)*(1 - 2*lamb**2))/((fc**2)*(np.pi**4)*l1*l2*lamb*np.sqrt(1 - lamb**2))
+    #                 rad_libre = ((2*(l1 + l2)*c0*delta_1/(l1*l2*fc))) + delta_2
+    #             sigma_2 = sigma2(f)
+    #             if f<f11 and f<fc/2 and rad_libre > sigma_2:
+    #                 rad_libre = sigma_2
+    #         elif (f11 > fc/2):
+    #             sigma_1 = sigma1(f)
+    #             sigma_2 = sigma2(f)
+    #             sigma_3 = sigma3(f)
+    #             if (f < fc) and (sigma_2 < sigma_3):
+    #                 rad_libre = sigma_2
+    #             elif (f > fc) and (sigma_1 < sigma_3):
+    #                 rad_libre = sigma_1
+    #             else:
+    #                 rad_libre = sigma_3
+    #         if rad_libre > 2:
+    #             rad_libre = 2 
+    #         if f < fc:
+    #             rad_forzada = radforzada(f)
+    #             eta_total = etatotal(f)
+    #             tao = abs((((2*ro0*c0)/(2*np.pi*f*m))**2)*(2*rad_forzada + (((l1 + l2)**2)/(l1**2 + l2**2))*np.sqrt(fc/f)*(rad_libre**2)/eta_total))
+    #             R_iso.append(round(float(-10*np.log10(tao)),1)) 
+    #         elif f == fc:
+    #             eta_total = etatotal(f)
+    #             tao = abs((((2*ro0*c0)/(2*np.pi*f*m))**2)*((np.pi*(rad_libre)**2)/(2*eta_total)))
+    #             R_iso.append(round(float(-10*np.log10(tao)),1))
+    #         elif f > fc:
+    #             eta_total = etatotal(f)
+    #             tao = abs((((2*ro0*c0)/(2*np.pi*f*m))**2)*((np.pi*fc*(rad_libre)**2)/(2*f*eta_total)))
+    #             R_iso.append(round(float(-10*np.log10(tao)),1))
+    #     R_iso = np.array(R_iso)
+    #     return R_iso
+    
+    # def sigma_libre():
         
-        # función [single_leaf] = Single_leaf_Davy(frequency, density, Young, Poisson, thickness, lossfactor, length, width) 
+    # def sigma_forzado():
+    
+    def iso_model(self, frequencies):
+        rho_0 = self.__density_air
+        c_0 = self.__vel_sound_air
+        l_x = self.__lx
+        l_y = self.__ly
+        t = self.__thickness
+        m = self.__mass_sup
+        c_l = np.sqrt(self.__young_module/self.__density) #vel long del material m/s
+        f_p = c_l/(5.5*self.__thickness)
+        f_11 = self.__freq_res
+        f_c = c_0/(1.8*c_l*t) 
+        f_c_eff = 0
+        tau = 0
+        reduction = []
         
         
+        for f in frequencies:
+            n_tot = self.__loss_factor + np.pi/(485*np.sqrt(f))
+            k_wave = 2*np.pi*f/c_0
+            lambda_f = np.sqrt(f/f_c)
+            
+            #factor de radiación para transmisión forzada 
+            if l_x > l_y:
+                lambda_upper = -0.964 - (0.5 + l_y/(np.pi*l_x))*np.log(l_y/l_x)+(5*l_y)/(2*np.pi*l_x)-1/(k_wave**2*4*np.pi*l_x*l_y)
+                sigma_f = 0.5*(np.log(k_wave*np.sqrt(l_x*l_y))-lambda_upper)
+            elif l_x <= l_y:
+                lambda_upper = -0.964 - (0.5 + l_x/(np.pi*l_y))*np.log(l_x/l_y)+(5*l_x)/(2*np.pi*l_y)-1/(k_wave**2*4*np.pi*l_x*l_y)
+                sigma_f = 0.5*(np.log(k_wave*np.sqrt(l_x*l_y))-lambda_upper)
+                
+            #factor de radiación para ondas de flexión libres
+            sigma_1 = 1/np.sqrt(1-f_c/f)
+            sigma_2 = 4*l_x*l_y*(f/c_0)**2
+            sigma_3 = np.sqrt((2*np.pi*f*(l_x+l_y))/(16*c_0))
+            
+            if f_11<=f_c/2:
+                if f>=f_c:
+                    if f<f_p:
+                        f_c_eff = f_c*(4.05*(t*f/c_l)+np.sqrt(1+(4.05*t*f/c_l)))
+                    else:
+                        f_c_eff = 2*f_c*(f/f_c)**3
+                        
+                    sigma_1 = 1/np.sqrt(1-f_c/f)
+                    sigma = sigma_1
+                elif f<f_c:
+                    delta_1 = ((1-lambda_f**2)*np.log((1+lambda_f)/(1-lambda_f))+2*lambda_f)/(4*np.pi**2*(1-lambda_f**2)**1.5)
+                    
+                    if f>f_c/2:
+                        delta_2 = 0
+                    else:
+                        delta_2 = (8*c_0**2*(1-2*lambda_f**2))/(f_c**2*np.pi**4*l_x*l_y*lambda_f*np.sqrt(1-lambda_f**2))
+                    
+                    sigma = ((2*(l_x+l_y)*c_0)/(l_x*l_y*f_c))*delta_1 + delta_2
+                    
+                    if f_11<f_c and f<f_11 and sigma > sigma_2:
+                        sigma = sigma_2
+            else:
+                if f<f_c and sigma_2<sigma3:
+                    sigma = sigma_2
+                elif f>f_c and sigma_1<sigma_3:
+                    sigma = sigma_1
+                else:
+                    sigma = sigma_3
+                
+                    
+            
+            if f>f_c:
+                tau = ((2*rho_0*c_0)/(2*np.pi*f*m))**2 * (np.pi*f_c*sigma**2)/(2*f*n_tot)
+            elif f==round(f_c,0):
+                tau = ((2*rho_0*c_0)/(2*np.pi*f*m))**2 * (np.pi*sigma**2)/(2*n_tot)
+            else:
+                tau = ((2*rho_0*c_0)/(2*np.pi*f*m))**2 * (2*sigma_f + ((l_x+l_y)**2/(l_x**2+l_y**2))*sqrt(f_c/f)*sigma**2/n_tot)
+            
+            r = -10*np.log10(tau)
+            reduction.append(r)
         
-        # función [rad] = Sigma(G, freq, width, length) 
-         
-        
-        # función [out] = shear(frequency, density, Young, Poisson, thickness) 
-       
+        return frequencies, reduction
